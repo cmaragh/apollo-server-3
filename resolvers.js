f2fe4f8b9af2651076b408e0533966f5898e416b
cmaragh <53223685+cmaragh@users.noreply.github.com>
2020-11-27 15:30:53 -0500

    Created schema and resolver for Link Model

--- resolvers.js
+++ resolvers.js
@@ -1,13 +1,7 @@
 const resolvers = {
   Query: {
-    async user(root, { id }, { models }) {
-      return models.User.findById(id);
-    },
-    async allRecipes(root, args, { models }) {
-      return models.Recipe.findAll();
-    },
-    async recipe(root, { id }, { models }) {
-      return models.Recipe.findById(id);
+    async allLinks(root, args, { models }) {
+      return models.Link.findAll();
     }
   }
 };
